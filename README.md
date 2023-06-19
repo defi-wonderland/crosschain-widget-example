d33f31e605b75a0c2ecf66449ea12f6a3571ae5b
turtlemoji <78362532+turtlemoji@users.noreply.github.com>
2023-06-19 16:54:51 -0300

    Release v1 (#7)

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# crosschain-widget-example-private
+# Cross-chain Governance Widget
+
+The Cross-chain Governance Widget is a user-friendly React component library that enables users to deploy and manage their Avatar's Zodiac Connext Module and easily build the transaction data required for executing desired actions through an `xcall` function in [Connext](https://docs.connext.network/).
+
+## Installation
+
+You can install the Crosschain Widget package via yarn. Make sure you have Node.js and yarn installed.
+
+```bash
+yarn add crosschain-widget
+```
+
+## Usage
+
+To use the Crosschain Widget in your React application, import the `ZodiacConnextWidget` component from the package and provide the required props.
+
+```jsx
+import { ZodiacConnextWidget } from "crosschain-widget";
+
+// Inside your React component
+const MyComponent = () => {
+  const handleTransaction = (tx) => {
+    // Handle the transaction data
+  };
+
+  return (
+    <ZodiacConnextWidget
+      originAddress="0x123abc..."
+      userChainId={1}
+      setTx={handleTransaction}
+      text="Open Widget Modal!"
+      modal={true}
+      provider={myProviderInstance}
+      lightTheme={true}
+    />
+  );
+};
+```
+
+Please note that you need to provide appropriate values for the `originAddress`, `userChainId`, `setTx`, and `provider` props according to your application's requirements.
+
+## Live example
+
+You can find the live code example [here](https://codesandbox.io/p/github/defi-wonderland/crosschain-widget-example)
---
+++ src/App.tsx
@@ -0,0 +1,97 @@
+import { useState, useEffect } from "react";
+import { ZodiacConnextWidget } from "crosschain-widget";
+import { providers } from "ethers";
+
+import "./App.css";
+
+function App() {
+  const [useLightTheme, setUseLightTheme] = useState(false);
+  const [userAddress, setUserAddress] = useState("");
+  const [userChainId, setUserChainId] = useState(1);
+  const [tx, setTx] = useState("");
+  const [signer, setSigner] = useState<providers.JsonRpcSigner | undefined>();
+  const [provider, setProvider] = useState<
+    providers.JsonRpcProvider | undefined
+  >();
+
+  const handleConnect = async () => {
+    if (window?.ethereum) {
+      const provider = new providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+
+      const signer = provider.getSigner();
+      const address = (await signer.getAddress()) || "";
+      const chainId = (await signer.getChainId()) || 1;
+      setSigner(signer);
+      setProvider(provider);
+      setUserChainId(chainId);
+      setUserAddress(address);
+    }
+  };
+
+  const detectChainChange = async () => {
+    window?.ethereum.on("chainChanged", handleConnect);
+  };
+
+  useEffect(() => {
+    detectChainChange();
+    return () => {
+      window.ethereum.removeListener("chainChanged", handleConnect);
+    };
+  }, []);
+
+  // autoconnect on load
+  useEffect(() => {
+    handleConnect();
+  }, []);
+
+  return (
+    <>
+      <h1>Crosschain Widget Example</h1>
+
+      <div className="card">
+        <button onClick={() => handleConnect()}>Connect</button>
+        <button onClick={() => setUseLightTheme(!useLightTheme)}>
+          Light theme: {useLightTheme.toString()}
+        </button>
+        <br />
+        <br />
+        <label>User Address: {userAddress}</label> <br />
+        <label>Origin chain Id: {userChainId}</label> <br />
+        <br />
+        {userAddress && userChainId && (
+          <ZodiacConnextWidget
+            originAddress={userAddress}
+            userChainId={userChainId}
+            text="Open crosschain widget"
+            modal={false}
+            setTx={setTx}
+            provider={provider}
+            lightTheme={useLightTheme}
+          />
+        )}
+        <br />
+        <br />
+        <textarea
+          value={tx}
+          onChange={(e) => setTx(e.target.value)}
+          style={{ width: 500, height: 250 }}
+          placeholder="{value: string; to: string; from: string; data: string;}"
+        />
+        <br />
+        {tx && (
+          <button
+            onClick={() => {
+              signer?.sendTransaction(JSON.parse(tx));
+            }}
+          >
+            Send tx
+          </button>
+        )}
+        <br />
+      </div>
+    </>
+  );
+}
+
+export default App;
